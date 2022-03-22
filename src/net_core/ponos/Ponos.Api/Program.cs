e466555271fdd8389e6d830f7120af3c1e55e569
Luis Raul Espinoza Barboza <respinozabarboza@gmail.com>
2022-03-21 21:02:21 -0700

    added serilog

--- src/net_core/ponos/Ponos.Api/Program.cs
+++ src/net_core/ponos/Ponos.Api/Program.cs
@@ -2,10 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Ponos.Api
 {
@@ -13,7 +12,34 @@ namespace Ponos.Api
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+               .AddEnvironmentVariables()
+               .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args)
+                    .Build()
+                    //.MigrateDatabase()
+                    .Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
